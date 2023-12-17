d1c3105996cc35b2375e28b80e597992decc4bdb
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-12-18 01:59:11 +0330

    static based of header was built

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+
 function Header() {
-    return (
-      <div>Header</div>
-    )
-  }
-  
-  export default Header
+  const [destination, setDestination] = useState("");
+  return (
+    <div className="header">
+      Home
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            type="text"
+            className="headerSearchInput"
+            placeholder="where to go?"
+            name="destination"
+            id="destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/05/06</div>
+        </div>
+        <div className="headerSearchItem">
+          <div className="optionDropDown">
+            1 adult &bull; 2 children &bull; 1 room
+          </div>
+        </div>
+        <div className="headerSearchItem headerSearchBtn">
+          <HiSearch className="heroIcon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
