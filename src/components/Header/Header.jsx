bf494e5e7f64a1ec662e2a7183ebbbd35b2354be
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-12-19 02:33:02 +0330

    optionDropDown became dynamic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import { MdLocationOn } from "react-icons/md";
-import { HiCalendar, HiSearch } from "react-icons/hi";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
 
 function Header() {
   const [destination, setDestination] = useState("");
+  const [openOption, setOpenOption] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+  const handleOptions = (type, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [type]: operation == "inc" ? prev[type] + 1 : prev[type] - 1,
+      };
+    });
+  };
   return (
     <div className="header">
       Home
@@ -25,12 +39,21 @@ function Header() {
           <div className="dateDropDown">2023/05/06</div>
         </div>
         <div className="headerSearchItem">
-          <div className="optionDropDown">
-            1 adult &bull; 2 children &bull; 1 room
+          <div id="optionDropDown" onClick={() => setOpenOption(!openOption)}>
+            {options.adult}adult &bull; {options["children"]} children &bull;
+            {options["room"]} room
           </div>
+          {openOption && (
+              <GuestOptionList
+                handleOptions={handleOptions}
+                options={options}
+              />
+            )}
         </div>
-        <div className="headerSearchItem headerSearchBtn">
-          <HiSearch className="heroIcon" />
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="heroIcon" />
+          </button>
         </div>
       </div>
     </div>
@@ -38,3 +61,56 @@ function Header() {
 }
 
 export default Header;
+
+function GuestOptionList({ options, handleOptions }) {
+  return (
+    <div className="guestOptions">
+      <GuestOptionItem
+        type="adult"
+        options={options}
+        minLimit={2}
+        handleOptions={handleOptions}
+      />
+      <GuestOptionItem
+        type="children"
+        options={options}
+        minLimit={1}
+        handleOptions={handleOptions}
+      />
+      <GuestOptionItem
+        type="room"
+        options={options}
+        minLimit={2}
+        handleOptions={handleOptions}
+      />
+    </div>
+  );
+}
+
+function GuestOptionItem({ options, type, minLimit, handleOptions }) {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">{type}</span>
+      <div className="optionCounter">
+        <button
+          className="optionCounterBtn"
+          disabled={options[type] < minLimit}
+          onClick={() => {
+            handleOptions(type, "dec");
+          }}
+        >
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">{options[type]}</span>
+        <button
+          className="optionCounterBtn"
+          onClick={() => {
+            handleOptions(type, "inc");
+          }}
+        >
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
