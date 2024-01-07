66219bf1aa05a2487c3990e62e37434620655f6b
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-01-07 20:52:26 +0330

    fetch data filter by searchparams

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -1,14 +1,28 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useSearchParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Loading from "./LocationList/Loading";
 
 function Hotels() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get("destination");
+  const { room } = JSON.parse(searchParams.get("options"));
+  // console.log(room);
+  const { isLoading, data } = useFetch(
+    "http://localhost:5000/hotels",
+    `q=${destination || ""}&accommodates_gte=${room ||1}`
+  );
+  console.log(isLoading,data);
+  if (isLoading) <Loading />;
+
   return (
     <div>
-        <div className="sidebar">
-            <Outlet/>
-        </div>
-        <div className="map">map</div>
+      <div className="sidebar">
+        {data.length}
+        <Outlet />
+      </div>
+      <div className="map">map</div>
     </div>
-  )
+  );
 }
 
-export default Hotels
+export default Hotels;
--- src/components/HotelsList.jsx
+++ src/components/HotelsList.jsx
@@ -1,6 +1,5 @@
 
 function HotelsList() {
-  // useFetch("http://localhost:5000/hotels",)
   return (
     <div>HotelsList</div>
   )
